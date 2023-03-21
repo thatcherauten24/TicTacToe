90d89c4d8035d7bc93ba01f5652d0791b1aefd3f
Charles Sharp <25charlessharp@usd266.com>
2023-03-21 12:30:36 -0500

    save

--- TicTacToeBoard.java
+++ TicTacToeBoard.java
@@ -1,18 +1,18 @@
-public class TicTacToeBoard{
-    public boolean a1 = true;
-    public boolean a2 = true;
-    public boolean a3 = true;
-    public boolean b1 = true;
-    public boolean b2 = true;
-    public boolean b3 = true;
-    public boolean c1 = true;
-    public boolean c2 = true;
-    public boolean c3 = true;
+public class TicTacToeBoard {
+    private boolean a1 = true;
+    private boolean a2 = true;
+    private boolean a3 = true;
+    private boolean b1 = true;
+    private boolean b2 = true;
+    private boolean b3 = true;
+    private boolean c1 = true;
+    private boolean c2 = true;
+    private boolean c3 = true;
     boolean isValidMove(int r, int c){
         if(r <= 3 && c <= 3 && r >= 1 && c >= 1){
-            if
+            
         }
-    }
+        }
 
     void makeMove(int r, int c, int player){
 
@@ -29,4 +29,5 @@ public class TicTacToeBoard{
     String toString(){
 
     }
+
 }
