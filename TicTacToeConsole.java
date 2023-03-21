20ed541e88331d30bc6970546a3aaf925f55003e
Thatcher Auten <thatcherauten24@gmail.com>
2023-03-21 12:03:29 -0500

    setting up

--- TicTacToeConsole.java
+++ TicTacToeConsole.java
@@ -1,1 +1,16 @@
-//Charles is gay
+import java.util.Scanner;
+public class TicTacToeConsole {
+
+
+
+
+
+    
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+
+
+        
+    }
+}
