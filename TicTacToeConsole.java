d64b36be8c25bc335a70da8aa2ba487a7485b859
Thatcher Auten <thatcherauten24@gmail.com>
2023-03-21 12:15:49 -0500

    testing other methods

--- TicTacToeConsole.java
+++ TicTacToeConsole.java
@@ -1,16 +1,14 @@
 import java.util.Scanner;
 public class TicTacToeConsole {
 
-
-
-
-
-    
     public static void main(String[] args) {
+        
+        getGameStatus();
+
         Scanner s = new Scanner(System.in);
         
 
 
-        
+        s.close();
     }
 }
