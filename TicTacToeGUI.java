dda6a0dcba5d3b12f66e6324221c33b569a2db8d
Danny Nguyen <25dnguyeuyen20@gamil.com>
2023-03-21 12:27:19 -0500

    Added a button

--- TicTacToeGUI.java
+++ TicTacToeGUI.java
@@ -1,3 +1,31 @@
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import java.awt.Button;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+
+
 public class TicTacToeGUI {
-    
+
+    public TicTacToeGUI(){
+        JFrame Frame = new JFrame();
+        JPanel pannel = new JPanel();
+        JButton button = new JButton("Charles is gay");
+        pannel.setBorder(BorderFactory.createEmptyBorder(30, 10, 30, 30));
+        pannel.setLayout(new GridLayout());
+        pannel.add(button);
+        Frame.add(pannel, BorderLayout.CENTER);
+        Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Frame.setTitle("Something works");
+        Frame.pack();
+        Frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new TicTacToeGUI();
+    }
 }
